fa3d93915deda21b8b4a206b2e8d72c57230b82b
Francerz <francerzam@hotmail.com>
2020-11-30 20:21:41 -0600

    Added CodeIgniter4 Psr7 Response emit helper

---
+++ src/Frameworks/CodeIgniter4.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Francerz\Utils\Frameworks;
+
+use Psr\Http\Message\ResponseInterface;
+
+class CodeIgniter4
+{
+    public static function outputResponsePsr7($ci_response, ResponseInterface $response)
+    {
+        $ci_response->setStatusCode($response->getStatusCode());
+
+        foreach ($response->getHeaders as $name => $values) {
+            $ci_response->setHeader($name, $values);
+        }
+
+        $ci_response->setBody((string)$response->getBody());
+    }
+}
